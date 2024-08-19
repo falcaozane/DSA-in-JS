8d8ea0b51b0c795b568f0f1be10da8471afcbc36
Zane Falcao <falcaozane@gmail.com>
2024-08-19 18:38:50 +0530

    Linked List

---
+++ LL/index.js
@@ -0,0 +1,120 @@
+class Node{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    }
+
+    insertAtHead(data){
+        const newNode = new Node(data);
+        newNode.next = this.head;
+        this.head = newNode;
+        this.size++;
+    }
+
+    insertAt(index,data){
+        if(index < 0 || index > this.size){
+            return "Index invalid"
+        }
+
+        if(index === 0){
+            this.insertAtHead(data)
+            return;
+        }
+
+        let newNode = new Node(data)
+        let temp = this.head;
+        for(let i=0;i<index-1;i++){
+            temp = temp.next;
+        }
+
+        newNode.next = temp.next;
+        temp.next = newNode;
+
+        this.size++;
+    }
+
+    print(){
+        let temp = this.head;
+        let result = "";
+        while(temp){
+            result += `${temp.data}->`;
+            temp=temp.next;
+        }
+        return result;
+    }
+
+    removeAtHead(){
+        if(this.isEmpty()){
+            return "List is already empty"
+        }
+        
+        this.head = this.head.next;
+        this.size--;
+    }
+
+    removeElement(data) {
+        if (this.isEmpty()) {
+            return "List is already empty";
+        }
+    
+        let current = this.head;
+        let prev = null;
+    
+        while (current) {
+            if (current.data === data) {
+                if (prev) {
+                    prev.next = current.next;
+                } else {
+                    // If prev is null, it means we're removing the head
+                    this.head = current.next;
+                }
+                this.size--;
+                return current.data;
+            }
+            prev = current;
+            current = current.next;
+        }
+    
+        return -1;  // Data not found
+    }
+    
+    searchElement(data){
+        let curr = this.head;
+        let index = 0;
+
+        while(curr){
+            if(curr.data === data){
+                return index;
+            }
+            index++;
+            curr = curr.next
+        }
+
+        return -1;
+    }
+
+    isEmpty(){
+        this.size === 0;
+    }
+}
+
+let list = new LinkedList()
+
+//console.log(list)
+list.insertAtHead(43);
+list.insertAtHead(50);
+list.insertAtHead(34);
+list.insertAt(2,46);
+console.log(list.print())
+list.removeAtHead() // 34 will be removed
+console.log(list.print())
+list.removeElement(46); // 46 will be removed
+console.log(list.searchElement(50)) // 0
+console.log(list.print())
