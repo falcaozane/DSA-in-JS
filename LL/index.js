df2eacdc5ec2abc64c49f9d90f0350011713f3db
Zane Falcao <falcaozane@gmail.com>
2024-08-21 21:00:06 +0530

    recursion

--- LL/index.js
+++ LL/index.js
@@ -9,7 +9,7 @@ class LinkedList{
     constructor(){
         this.head = null;
         this.size = 0;
-    }
+    } 
 
     insertAtHead(data){
         const newNode = new Node(data);
@@ -84,7 +84,31 @@ class LinkedList{
     
         return -1;  // Data not found
     }
-    
+
+    deleteAt(index){
+        if(index < 0 || index > this.size){
+            return "Index invalid"
+        }
+
+        if(index === 0){
+            this.removeAtHead()
+            return;
+        }
+
+        let current = this.head;
+
+        for(let i=0;i<index-1 && current!==null;i++){
+            current = current.next;
+        }
+        
+        if(current==null||current.next==null) return -1;
+
+        let nodeToDelete = current.next;
+        current.next = nodeToDelete.next;
+        this.size--;
+        return nodeToDelete.data;
+    }
+
     searchElement(data){
         let curr = this.head;
         let index = 0;
@@ -113,8 +137,10 @@ list.insertAtHead(50);
 list.insertAtHead(34);
 list.insertAt(2,46);
 console.log(list.print())
-list.removeAtHead() // 34 will be removed
+list.removeAtHead() // 34 will be removed it is delete element at begin
 console.log(list.print())
-list.removeElement(46); // 46 will be removed
+list.deleteAt(4)
+console.log(list.print());
+list.removeElement(46); // 46 will be removed it is delete element by value
 console.log(list.searchElement(50)) // 0
 console.log(list.print())
