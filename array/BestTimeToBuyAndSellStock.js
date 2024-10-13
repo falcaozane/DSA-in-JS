49422f84983b46f8851b6661cf8884048c23a430
Falcao Zane Vijay <112317470+falcaozane@users.noreply.github.com>
2024-10-13 10:26:19 +0000

    sliding window

---
+++ array/BestTimeToBuyAndSellStock.js
@@ -0,0 +1,22 @@
+// link : https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+var maxProfit = function(prices) {
+    let i=0;
+    let j=0;
+    let maxP = 0;
+    while(j<prices.length){
+        if(prices[j]>prices[i]){
+            maxP = Math.max(maxP, prices[j]-prices[i]);
+        }
+        else{
+            i=j;
+        }
+        j++;
+    }
+
+    return maxP;
+};
+
+
+let prices = [7,1,5,3,6,4]
+
+console.log(maxProfit(prices));
