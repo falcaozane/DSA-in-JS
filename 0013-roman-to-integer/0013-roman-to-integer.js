1ecd96d7bdab2eb61a34468be941b2011e3f75f3
Falcao Zane Vijay <112317470+falcaozane@users.noreply.github.com>
2024-10-21 16:48:50 +0530

    Time: 10 ms (98.13%), Space: 55.7 MB (18.74%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -2,30 +2,30 @@
  * @param {string} s
  * @return {number}
  */
+ var num = function(c) {
+    switch(c) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        default: return 1000;
+    }
+}
 var romanToInt = function(s) {
-    const sym = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
+    let sum = 0, index = 0;
 
-    let result = 0;
-
-    for (let i = 0; i < s.length; i++) {
-        const cur = sym[s[i]];
-        const next = sym[s[i + 1]];
-
-        if (cur < next) {
-            result += next - cur;
-            i++;
+    while(index < s.length - 1) {
+        if(num(s[index]) < num(s[index + 1])) {
+            sum -= num(s[index]);
         } else {
-            result += cur;
+            sum += num(s[index]);
         }
+        index++;
     }
 
-    return result;
+    sum += num(s[index]);
+
+    return sum;
 };
