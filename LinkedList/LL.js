8421ab819486d7148b376a9002d3a973db86b58a
Falcao Zane Vijay <112317470+falcaozane@users.noreply.github.com>
2024-10-22 14:02:51 +0000

    LL

--- LinkedList/LL.js
+++ LinkedList/LL.js
@@ -11,6 +11,37 @@ class LinkedList {
         this.size = 0;
     }
 
-    
+    isEmpty(){
+        return this.size ===0;
+    }
+
+    getSize(){
+        return this.size;
+    }
+
+    prepend(value){
+        //const newNode = new Node(value);
+        const newNode = new Node();
+        newNode.data = value;
+        
+        newNode.next = this.head;
+        this.head = newNode;
+        this.size++;
+    }
+
+    print(){
+        let temp = this.head;
+        while(temp){
+            process.stdout.write(temp.data + " -> ");
+            temp = temp.next;
+        }
+        console.log("null");
+    }
 }
 
+let list = new LinkedList()
+
+list.prepend(43);
+list.prepend(45);
+list.prepend(46);
+list.print();
